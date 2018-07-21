9321115f1b41f23ccf077cb28e6d3726c014f6df
TobiasWeis <mail@tobias-weis.de>
2018-07-21 15:21:46 +0200

    refactored queue creation

--- server-websocket/main.py
+++ server-websocket/main.py
@@ -10,28 +10,25 @@ from multiprocessing import Process, Queue
 
 _debug = False
 
-ql = Queue()
-qs = Queue()
-qs2 = Queue()
-qb = Queue()
-qb2 = Queue()
+
+# TODO: axis-mapping should be OOP and automatic!
 
 try:
     from robot.Linear import *
-    linear = Linear(ql, verbose=False)
+    linear = Linear(Queue(), verbose=False)
     linear.start()
 
     from robot.Servo import *
-    servo = Servo(qs, 7, verbose=False)
+    servo = Servo(Queue(), 7, verbose=False)
     servo.start()
-    servo2 = Servo(qs2, 11, verbose=False)
+    servo2 = Servo(Queue(), 11, verbose=False)
     servo2.start()
 
     from robot.Binary import *
-    binary = Binary(qb, 13, axis="A", verbose=False)
+    binary = Binary(Queue(), 13, axis="A", verbose=False)
     binary.start()
 
-    binary2 = Binary(qb2,15, axis="B", verbose=False)
+    binary2 = Binary(Queue(),15, axis="B", verbose=False)
     binary2.start()
 except Exception, e:
     print("Could not import robot")
@@ -44,26 +41,17 @@ CORS(app)
 socketio = SocketIO(app)
 
 
-def map_dist_angle(distance, angle):
-    """
-    map distance and angle values to independent x/y axis values,
-    angle is in degrees, distance is normalized to diameter of joystick circle
-    """
-    x = distance * math.cos(math.radians(angle))
-    y = distance * math.sin(math.radians(angle))
-
-    return x,y
-
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @socketio.on_error_default
 def default_error_handler(e):
     print "======================= ERROR"
-    print(request.event["message"]) # "my error event"
-    print(request.event["args"])    # (data,)"
+    print(request.event["message"])
+    print(request.event["args"])
+
 
 @socketio.on('control', namespace='/control')
 def control(message):
@@ -72,19 +60,19 @@ def control(message):
         x = data["left"][0]
         y = data["left"][1]
         if _debug: print "[Server] Left: ",x,",",y
-        ql.put(("left",x,y))
+        linear.q.put(("left",x,y))
     elif "right" in data.keys():
         x = data["right"][0]
         y = data["right"][1]
         if _debug: print "[Server] Right: ",x,",",y
-        qs.put(("right",x,y))
-        qs2.put(("right",y,x))
+        servo.q.put(("right",x,y))
+        servo2.q.put(("right",y,x))
     elif "A" in data.keys():
         if _debug: print "[Server] A"
-        qb.put(("A",1,0))
+        binary.q.put(("A",1,0))
     elif "B" in data.keys():
         if _debug: print "[Server] B"
-        qb2.put(("B",1,0))
+        binary2.q.put(("B",1,0))
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", debug=True, use_reloader=False)
